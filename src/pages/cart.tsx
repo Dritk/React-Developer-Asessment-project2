dd55f68780269e1fe31cf9e14e1a26c46272dcfe
Dritk <dritkarmacharya@gmail.com>
2026-03-07 12:03:44 +0545

    Add cart page with functionality

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { type CartResponse } from "../types/cart";
 
 const Cart = () => {
-  return <div>cart</div>;
-};
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const quantity = searchParams.get("q");
+  const [cart, setCart] = useState<CartResponse>();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await axios.post("https://dummyjson.com/carts/add", {
+          userId: 1,
+          products: [{ id: Number(id), quantity: Number(quantity) }],
+        });
+        setCart(data.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, [id, quantity]);
+  console.log({ cart });
+  return (
+    <div>
+      {cart?.products.map((p) => (
+        <div key={p.id}>
+          <p>{p.title}</p>
+          <p>{p.price}</p>
+          <p>{p.quantity}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Cart;
---
+++ src/types/cart.ts
@@ -0,0 +1,22 @@
+
+
+export interface CartResponse {
+  id: number
+  products: Product[]
+  total: number
+  discountedTotal: number
+  userId: number
+  totalProducts: number
+  totalQuantity: number
+}
+
+export interface Product {
+  id: number
+  title: string
+  price: number
+  quantity: number
+  total: number
+  discountPercentage: number
+  discountedPrice: number
+  thumbnail: string
+}
